5629f8436676a0547a3196c9a53e32e9716bc6dc
Andre De Santana <ads.andredesantana@gmail.com>
2021-01-14 09:38:44 -0500

    Bootstraping Devise

--- app/javascript/components/NewRecipe.jsx
+++ app/javascript/components/NewRecipe.jsx
@@ -7,7 +7,8 @@ class NewRecipe extends React.Component {
     this.state = {
       name: "",
       ingredients: "",
-      instruction: ""
+      instruction: "",
+      image: ""
     };
 
     this.onChange = this.onChange.bind(this);
@@ -28,15 +29,16 @@ class NewRecipe extends React.Component {
   onSubmit(event) {
     event.preventDefault();
     const url = "/api/v1/recipes/create";
-    const { name, ingredients, instruction } = this.state;
+    const { name, ingredients, instruction, image } = this.state;
 
-    if (name.length == 0 || ingredients.length == 0 || instruction.length == 0)
+    if (name.length == 0 || ingredients.length == 0 || instruction.length == 0 || image.length == 0)
       return;
 
     const body = {
       name,
       ingredients,
-      instruction: instruction.replace(/\n/g, "<br> <br>")
+      instruction: instruction.replace(/\n/g, "<br> <br>"),
+      image
     };
 
     const token = document.querySelector('meta[name="csrf-token"]').content;
@@ -67,46 +69,31 @@ class NewRecipe extends React.Component {
               Add a new recipe to our awesome recipe collection.
             </h1>
             <form onSubmit={this.onSubmit}>
+
               <div className="form-group">
                 <label htmlFor="recipeName">Recipe name</label>
-                <input
-                  type="text"
-                  name="name"
-                  id="recipeName"
-                  className="form-control"
-                  required
-                  onChange={this.onChange}
-                />
+                <input type="text" name="name" id="recipeName" className="form-control" required onChange={this.onChange} />
               </div>
+
               <div className="form-group">
                 <label htmlFor="recipeIngredients">Ingredients</label>
-                <input
-                  type="text"
-                  name="ingredients"
-                  id="recipeIngredients"
-                  className="form-control"
-                  required
-                  onChange={this.onChange}
-                />
+                <input type="text" name="ingredients" id="recipeIngredients" className="form-control" required onChange={this.onChange} />
                 <small id="ingredientsHelp" className="form-text text-muted">
                   Separate each ingredient with a comma.
                 </small>
               </div>
+
+              <div className="form-group">
+                <label htmlFor="recipeImages">Picture Link</label>
+                <input type="text" name="ingredients" id="recipeImage" className="form-control" required onChange={this.onChange} />
+              </div>
+
               <label htmlFor="instruction">Preparation Instructions</label>
-              <textarea
-                className="form-control"
-                id="instruction"
-                name="instruction"
-                rows="5"
-                required
-                onChange={this.onChange}
-              />
-              <button type="submit" className="btn custom-button mt-3">
-                Create Recipe
-              </button>
-              <Link to="/recipes" className="btn btn-link mt-3">
-                Back to recipes
-              </Link>
+              <textarea className="form-control" id="instruction" name="instruction" rows="5" required onChange={this.onChange} />
+
+              <button type="submit" className="btn custom-button mt-3" > Create Recipe </button>
+              <br />
+              <Link to="/recipes" className="btn btn-link mt-3"> Back to recipes </Link>
             </form>
           </div>
         </div>
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,74 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-9.times do |i|
-  Recipe.create(
-    name: "Recipe #{i + 1}",
-    ingredients: '227g tub clotted cream, 25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
-    instruction: 'In a medium saucepan, stir the clotted cream, butter, and cornflour over a low-ish heat and bring to a low simmer. Turn off the heat and keep warm.'
-  )
-end
-
-# do
+# 9.times do |i|
 #   Recipe.create(
-#     name: 'Fruit Salade Nature',
-#     ingredients: 'Strawberry, Kiwi, Mango, Bananas',
-#     instruction: 'Put Yougurt and Enjoy!',
-#     image: 'images/food1.jpg'
+#     name: "Recipe #{i + 1}",
+#     ingredients: '227g tub clotted cream, 25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
+#     instruction: 'In a medium saucepan, stir the clotted cream, butter, and cornflour over a low-ish heat and bring to a low simmer. Turn off the heat and keep warm.'
 #   )
 # end
+
+
+Recipe.create(
+  name: 'Fruit Salade Nature',
+  ingredients: 'Strawberry, Kiwi, Mango, Bananas',
+  instruction: 'Put Yougurt and Enjoy!',
+  image: 'http://drive.google.com/uc?export=view&id=1hxFIIQ7hfHIbv_IINLlPX7Ijc39-EEjV'
+)
+
+Recipe.create(
+  name: 'Fruit Salade Nature',
+  ingredients: 'Strawberry, Kiwi, Mango, Bananas',
+  instruction: 'Put Yougurt and Enjoy!',
+  image: 'http://drive.google.com/uc?export=view&id=1SZfGncZdp_KoXYzLVTVPMF2-KfrqQDXd'
+)
+
+Recipe.create(
+  name: 'Fruit Salade Nature',
+  ingredients: 'Strawberry, Kiwi, Mango, Bananas',
+  instruction: 'Put Yougurt and Enjoy!',
+  image: 'http://drive.google.com/uc?export=view&id=1sd7y0Tau5jjjgoHmNR0-TZkN2tp1LBW7'
+)
+
+Recipe.create(
+  name: 'Fruit Salade Nature',
+  ingredients: 'Strawberry, Kiwi, Mango, Bananas',
+  instruction: 'Put Yougurt and Enjoy!',
+  image: 'http://drive.google.com/uc?export=view&id=1m8gwMO5w0iKkC7ztyzX7c408o5YseSlE'
+)
+
+Recipe.create(
+  name: 'Fruit Salade Nature',
+  ingredients: 'Strawberry, Kiwi, Mango, Bananas',
+  instruction: 'Put Yougurt and Enjoy!',
+  image: 'http://drive.google.com/uc?export=view&id=1of6mdA24EpaMOfJ9_FivqsFSiV8NUeoc'
+)
+
+Recipe.create(
+  name: 'Fruit Salade Nature',
+  ingredients: 'Strawberry, Kiwi, Mango, Bananas',
+  instruction: 'Put Yougurt and Enjoy!',
+  image: 'http://drive.google.com/uc?export=view&id=1ijfuUxiSNoqluFphCU6TL-Y853Sg9xRd'
+)
+
+Recipe.create(
+  name: 'Fruit Salade Nature',
+  ingredients: 'Strawberry, Kiwi, Mango, Bananas',
+  instruction: 'Put Yougurt and Enjoy!',
+  image: 'http://drive.google.com/uc?export=view&id=1w59gt-Kb5ruA0iDZTxv-uUk11EMaZBYm'
+)
+
+Recipe.create(
+  name: 'Fruit Salade Nature',
+  ingredients: 'Strawberry, Kiwi, Mango, Bananas',
+  instruction: 'Put Yougurt and Enjoy!',
+  image: 'http://drive.google.com/uc?export=view&id=1uoNDRtpzmQGkcw2ywusHWRcjDSQLxveP'
+)
+
+Recipe.create(
+  name: 'Fruit Salade Nature',
+  ingredients: 'Strawberry, Kiwi, Mango, Bananas',
+  instruction: 'Put Yougurt and Enjoy!',
+  image: 'http://drive.google.com/uc?export=view&id=1PfYm9aYuOEVwUowIGMn1Ju6lSFyUyN8A'
+)
