f62e327daac51b690dba2b7d2152f4c3ef70680e
Andre De Santana <ads.andredesantana@gmail.com>
2021-01-21 09:06:12 -0500

    Configuring routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :user
   namespace :api do
     namespace :v1 do
       get 'recipes/index'
-      post 'recipes/create'
-      get '/show/:id', to: 'recipes#show'
-      delete '/destroy/:id', to: 'recipes#destroy'
+      resources :recipes, except: [:new, :edit]
     end
   end
+
+  
   root 'homepage#index'
   get '/*path' => 'homepage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
