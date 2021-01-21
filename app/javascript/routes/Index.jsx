417724f923a191e885eb635a8c2b2c1cc6dd5c61
Andre De Santana <ads.andredesantana@gmail.com>
2021-01-21 09:05:17 -0500

    Updating the Routes with the new Components

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -4,14 +4,16 @@ import Home from "../components/Home";
 import Recipes from "../components/Recipes";
 import Recipe from "../components/Recipe";
 import NewRecipe from "../components/NewRecipe";
+import EditRecipe from "../components/EditRecipe";
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/recipes" exact component={Recipes} />
-      <Route path="/recipe/:id" exact component={Recipe} />
-      <Route path="/recipe" exact component={NewRecipe} />
+      <Route path="/recipes/new" exact component={NewRecipe} />
+      <Route path="/recipes/:id" exact component={Recipe} />
+      <Route path="/recipes/:id/edit" exact component={EditRecipe} />
     </Switch>
   </Router>
 );
